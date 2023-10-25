1607a9f44bce94d46fe8d2b6ceba501bad739163
rgonzo1355 <rgonzo1355.gmail.com>
2023-10-24 21:27:04 -0700

    ops challenge

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Hello World 
+# Author:                       Rodolfo Gonzalez
+# Date of latest revision:      10/24/2023
+# Purpose:                      First Ops Challenge
+
+# Declaration of variables
+my_variable="Hello World"
+Perfection="I am workingperfectly."
+
+# Declaration of functions
+
+
+# Main
+
+echo $my_variable
+echo $Perfection
+
+# End
