99b1cb64ccd0f445bcc35b1ad60131233823c83e
rgonzo1355 <rgonzo1355.gmail.com>
2023-10-26 12:56:57 -0700

    Added notes in code

--- VariablesR.sh
+++ VariablesR.sh
@@ -6,20 +6,20 @@
 # Purpose:                      Variables review 
 
 
-#  Declaration of Variables
-num1=25
+# Declaration of Variables
+num1=25 # variable assigned to num1 is 25.
 num2=5
 
-
-#  Declaration funtion
-add_some_numbers() {
-    sum=$(($num1 + $num2))
-    echo $sum
+# Declaration of Function (Allows script to be broken down to manageable and reusable pieces)
+add_some_numbers() { # Use empty () when defining functions, even if they don't take parameters.
+    sum=$(($num1 + $num2))  # Calculate the sum of num1 and num2
+    echo $sum  # Print the sum to the console (use $sum to reference the variables)
 }
 
-# Main
-# Call the function 
-add_some_numbers
+# Main (where the execution begins)
+# Call the function
+add_some_numbers  # Execute the add_some_numbers function
+
 
 
 # End
